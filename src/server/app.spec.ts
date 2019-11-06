43a71af0f2353c72e81170aa73c4b47c08c9fdae
Stefan Cosquer <stefan.cosquer@niji.fr>
2019-11-06 15:18:21 +0100

    [tech] refactoring and tests

---
+++ src/server/app.spec.ts
@@ -0,0 +1,122 @@
+import axios from "axios";
+import App from "./app";
+
+jest.mock("axios");
+
+describe("App", () => {
+
+  let res;
+
+  beforeEach(() => {
+    jest.clearAllMocks().resetModules();
+    res = {
+      status: jest.fn(() => res),
+      set: jest.fn(() => res),
+      send: jest.fn(() => res),
+    } as any;
+  });
+
+  it("should be configured", () => {
+    const app = new App(0, 10);
+    expect(app.app).not.toBeNull();
+    expect(app.wss).not.toBeNull();
+  });
+
+  it("should listen on chosen port", () => {
+    const app = new App(1234, 10);
+    app.app.listen = jest.fn();
+    app.listen();
+    expect(app.app.listen.mock.calls[0][0]).toBe(1234);
+  });
+
+  it("should proxy successful request and notify refresh", async () => {
+    const app = new App(1234, 10);
+    const refresh = app.refresh = jest.fn();
+
+    const req = {
+      method: "GET",
+      url: "/proxy/example.com/api/v1",
+      headers: [],
+    } as any;
+
+    (axios.request as jest.Mock).mockResolvedValue({
+      status: 200,
+      statusText: "OK",
+    });
+
+    await app.proxy(req, res);
+
+    expect((axios.request as jest.Mock).mock.calls[0][0].method).toBe("get");
+    expect((axios.request as jest.Mock).mock.calls[0][0].url).toBe("https://example.com/api/v1");
+    expect(res.status.mock.calls[0][0]).toBe(200);
+    expect(app.exchanges.length).toBe(1);
+    expect(refresh.mock.calls.length).toBe(2);
+  });
+
+  it("should proxy unsuccessful request and notify refresh", async () => {
+    const app = new App(1234, 10);
+    const refresh = app.refresh = jest.fn();
+
+    const req = {
+      method: "GET",
+      url: "/proxy/example.com/api/v1",
+      headers: [],
+    } as any;
+
+    (axios.request as jest.Mock).mockRejectedValue({
+      response: {
+        status: 400,
+        statusText: "Bad request",
+      },
+    });
+
+    await app.proxy(req, res);
+
+    expect((axios.request as jest.Mock).mock.calls[0][0].method).toBe("get");
+    expect((axios.request as jest.Mock).mock.calls[0][0].url).toBe("https://example.com/api/v1");
+    expect(res.status.mock.calls[0][0]).toBe(400);
+    expect(app.exchanges.length).toBe(1);
+    expect(refresh.mock.calls.length).toBe(2);
+  });
+
+  it("should proxy unhandled error and notify refresh", async () => {
+    const app = new App(1234, 10);
+    const refresh = app.refresh = jest.fn();
+
+    const req = {
+      method: "GET",
+      url: "/proxy/example.com/api/v1",
+      headers: [],
+    } as any;
+
+    (axios.request as jest.Mock).mockRejectedValue({ });
+
+    await app.proxy(req, res);
+
+    expect((axios.request as jest.Mock).mock.calls[0][0].method).toBe("get");
+    expect((axios.request as jest.Mock).mock.calls[0][0].url).toBe("https://example.com/api/v1");
+    expect(res.status.mock.calls[0][0]).toBe(500);
+    expect(app.exchanges.length).toBe(1);
+    expect(refresh.mock.calls.length).toBe(2);
+  });
+
+  it("should store no more exchanges than allowed by capacity", async () => {
+    const app = new App(1234, 10);
+    const refresh = app.refresh = jest.fn();
+
+    const req = {
+      method: "GET",
+      url: "/proxy/example.com/api/v1",
+      headers: [],
+    } as any;
+
+    (axios.request as jest.Mock).mockResolvedValue({ });
+
+    for (let i = 0; i < 20; i++) {
+      await app.proxy(req, res);
+    }
+
+    expect(app.exchanges.length).toBe(10);
+    expect(refresh.mock.calls.length).toBe(40);
+  });
+});
---
+++ src/server/app.ts
@@ -0,0 +1,95 @@
+import axios, { AxiosError, Method } from "axios";
+import * as bodyParser from "body-parser";
+import express, { Request, Response } from "express";
+import expressWs from "express-ws";
+import { performance } from "perf_hooks";
+import { Exchange } from "../model/exchange";
+
+export default class App {
+
+  public wss;
+  public app;
+
+  public exchanges = new Array<Exchange>();
+
+  public constructor(private port, private capacity) {
+    this.wss = expressWs(express());
+    this.app = this.wss.app;
+
+    this.initializeMiddlewares();
+    this.initializeProxy();
+    this.initializeWebsocket();
+  }
+
+  public listen() {
+    this.app.listen(this.port);
+    process.stdout.write(`Server listening on ${this.port}\n`);
+  }
+
+  public proxy(req: Request, res: Response) {
+    const exchange: Exchange = {
+      id: performance.now().toString(16).replace(".", ""),
+      request: {
+        time: Date.now(),
+        method: req.method.toLowerCase(),
+        url: req.url.replace("/proxy/", "https://"),
+        headers: req.headers,
+        data: typeof req.body === "string" ?
+          (req.header("content-type") === "application/json" ? JSON.parse(req.body) : req.body) :
+          null,
+      },
+    };
+    delete exchange.request.headers.host;
+    delete exchange.request.headers.connection;
+    delete exchange.request.headers["accept-encoding"];
+    this.exchanges.unshift(exchange);
+    this.exchanges.length = Math.min(this.exchanges.length, this.capacity);
+    this.refresh();
+    return axios.request({
+      method: exchange.request.method as Method,
+      headers: exchange.request.headers,
+      url: exchange.request.url,
+      data: exchange.request.data,
+    }).then((pResp) => {
+      exchange.response = {
+        time: Date.now(),
+        status: pResp.status,
+        text: pResp.statusText,
+        headers: pResp.headers,
+        data: pResp.data,
+      };
+    }).catch((reason: AxiosError) => {
+      exchange.response = {
+        time: Date.now(),
+        status: (reason.response && reason.response.status) || 500,
+        text: (reason.response && reason.response.statusText) || "Internal error",
+        headers: (reason.response && reason.response.headers) || {},
+        data: (reason.response && reason.response.data) || null,
+      };
+    }).finally(() => {
+      res.status(exchange.response.status).set(exchange.response.headers).send(exchange.response.data);
+      this.refresh();
+    });
+  }
+
+  public refresh() {
+    this.wss.getWss().clients.forEach((ws) => this.notify(ws));
+  }
+
+  private notify(ws) {
+    ws.send(JSON.stringify(this.exchanges));
+  }
+
+  private initializeMiddlewares() {
+    this.app.use(bodyParser.text({ type: "*/*"}));
+    this.app.use(express.static("dist"));
+  }
+
+  private initializeProxy() {
+    this.app.all("/proxy/*", (req: Request, res: Response) => this.proxy(req, res));
+  }
+
+  private initializeWebsocket() {
+    this.app.ws("/", (ws) => this.notify(ws));
+  }
+}
